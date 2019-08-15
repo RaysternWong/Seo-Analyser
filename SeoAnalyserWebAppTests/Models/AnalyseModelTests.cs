90d383437f4259d219e995c9ae30ac7b874dbdb0
raystern <raysternwong@gmail.com>
2019-08-15 09:43:44 +0800

    Removed redundant pages

--- SeoAnalyserWebAppTests/Models/AnalyseModelTests.cs
+++ SeoAnalyserWebAppTests/Models/AnalyseModelTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SeoAnalyserWebApp.Models;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SeoAnalyserWebApp.Models.Tests
 {
